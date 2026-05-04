02201bff5cba45a718b98c8830d1be36859acdd1
강은숙 <kangeunsuk68@gmail.com>
2026-05-04 13:03:15 +0900

    Update routes.tsx

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,24 +1,15 @@
 import { createBrowserRouter } from "react-router";
-import { HomePage } from "./pages/HomePage";
-import { BookMaker } from "./pages/BookMaker";
-import { MusicStudio } from "./pages/MusicStudio";
-import { ShortsCreator } from "./pages/ShortsCreator";
+
+function HomePage() {
+  return <div style={{padding: '40px', textAlign: 'center'}}>
+    <h1>퀸즈미라클 크리에이티브</h1>
+    <p>곧 오픈합니다!</p>
+  </div>;
+}
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
-  },
-  {
-    path: "/book",
-    element: <BookMaker />,
-  },
-  {
-    path: "/music",
-    element: <MusicStudio />,
-  },
-  {
-    path: "/shorts",
-    element: <ShortsCreator />,
   },
 ]);
